c4e0a67b67c2a6b04fb97d9d931d694c2b5a6bae
Anjelina Siallagan <anjelinasiallagan@gmail.com>
2022-02-09 14:04:52 +0700

    my-react-native-bootcamp

---
+++ Tugas2/string1.js
@@ -0,0 +1,74 @@
+{
+var word = 'Javascript';
+var second = 'is';
+var third = 'awesome';
+var fourth = 'and';
+var fifth = 'I';
+var sixth = 'love';
+var seventh = 'it!';
+
+console.log(word + " " + second + " " + third + " " + fourth + " " + fifth + " " + sixth + " " + seventh);
+
+}
+
+{
+    var sentence = "I am going to be React Native Developer"; 
+
+var exampleFirstWord = sentence[0] ; 
+var exampleSecondWord = sentence[2] + sentence[3]  ; 
+var thirdWord = sentence[4] + sentence[5] + sentence [6] + sentence[7] + sentence[8] + sentence [9]; 
+var fourthWord = sentence[11] + sentence[12]; 
+var fifthWord = sentence[14] + sentence[15]; ; // lakukan sendiri 
+var sixthWord = sentence[17] + sentence[18] + sentence [19] + sentence[20] + sentence[21]; // lakukan sendiri 
+var seventhWord = sentence[22] + sentence[23] + sentence [24] + sentence[25] + sentence[26] + sentence [27] + sentence [28]; // lakukan sendiri 
+var eighthWord = sentence[30] + sentence[31] + sentence [32] + sentence[33] + sentence[34] + sentence [35] + sentence [36] +sentence [37] + sentence [38]; // lakukan sendiri 
+
+console.log('First Word: ' + exampleFirstWord); 
+console.log('Second word : ' + exampleSecondWord);
+console.log('Third word : ' +  thirdWord);
+console.log('fourth word : ' + fourthWord);
+console.log('fifth word : ' + fifthWord);
+console.log('sixth word : '+ sixthWord);
+console.log('seventh word : ' + seventhWord);
+console.log('eitht word : ' + eighthWord);
+
+
+}
+
+{
+    var sentence2 = 'wow JavaScript is so cool'; 
+
+var exampleFirstWord2 = sentence2.substring(0, 3); 
+var secondWord2 = sentence2.substring(4,14); // do your own! 
+var thirdWord2 = sentence2.substring(15,17); // do your own! 
+var fourthWord2  = sentence2.substring(18,20); // do your own! 
+var fifthWord2 = sentence2.substring(21,25); // do your own! 
+
+console.log('First Word: ' + exampleFirstWord2); 
+console.log('Second Word: ' + secondWord2); 
+console.log('Third Word: ' + thirdWord2); 
+console.log('Fourth Word: ' + fourthWord2); 
+console.log('Fifth Word: ' + fifthWord2);
+}
+
+{
+    var sentence3 = 'wow JavaScript is so cool'; 
+
+var exampleFirstWord3 = sentence3.substring(0, 3); 
+var secondWord3 = sentence3.substring(4,14); // do your own! 
+var thirdWord3 = sentence2.substring(15,17); // do your own! 
+var fourthWord3 = sentence2.substring(18,20); // do your own! 
+var fifthWord3 = sentence2.substring(21,25); // do your own! 
+
+var firstWordLength = exampleFirstWord3.length  
+var secondWordLength = secondWord3.length
+var thirdWordlength = thirdWord3.length
+var fourthWordlength = fourthWord3.length
+var fifthWordlength = fifthWord3.length
+// lanjutkan buat variable lagi di bawah ini 
+console.log('First Word: ' + exampleFirstWord3 + ', with length: ' + firstWordLength); 
+console.log('Second Word: ' + secondWord3 + ',with length:' + secondWordLength); 
+console.log('Third Word: ' + thirdWord3 + 'with length:' + thirdWordlength); 
+console.log('Fourth Word: ' + fourthWord3 + 'with length:' + fourthWordlength); 
+console.log('Fifth Word: ' + fifthWord3 + 'with length'+ fifthWordlength); 
+}
