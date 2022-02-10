97e337a9f6421e0ef61449304cbd8cb677d7d3de
Anjelina Siallagan <anjelinasiallagan@gmail.com>
2022-02-10 12:25:35 +0700

    my-react-native-bootcamp

---
+++ Tugaslooping/looping.js
@@ -0,0 +1,32 @@
+
+    var tittle = ("LOOPING PERTAMA");
+        console.log(tittle)
+
+    var flag = 2;
+    while(flag <= 20){
+        console.log(flag + "-I Love Coding");
+        flag +=2;
+        
+}
+    var tittle2 = ("LOOPING KEDUA");
+        console.log(tittle2)
+    var flag = 20;
+    while(flag >= 4){
+        console.log(flag + "-I Will Become a mobile developer");
+        flag -=2;
+    }
+
+    var judul = ("Looping menggunakan for");
+        console.log(judul)
+
+    var a = ("santai");
+    var b = ("Berkualitas");
+    var c = ("I love coding");
+    for(var flag = 1; flag <=20;flag++){
+        if(flag +=1 );
+        console.log(flag + "-" + a);
+    }
+      
+    
+
+
